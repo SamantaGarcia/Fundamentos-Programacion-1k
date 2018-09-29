d282981122be9bebc347dd198c1d56e8d84f942e
1k <1k@iMac-de-iMac13.local>
2018-09-28 19:41:55 -0500

    ejer4

---
+++ unidad3/tarea2/ejercicio4/ejercicio4.js
@@ -0,0 +1,7 @@
+var cesarhueleacola = document.querySelector("#calcular");
+cesarhueleacola.addEventListener("click", calcular);
+
+function calcular(){
+  var vel = document.querySelector("#velocidad");
+  var tie = document.querySelector()
+}
