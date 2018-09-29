de53693cc452fb799cc13c9f0f107ff2a0357695
1k <1k@iMac-de-iMac13.local>
2018-09-28 19:39:30 -0500

    ejer1

--- unidad3/tarea2/ejercicio1/calculadora.js
+++ unidad3/tarea2/ejercicio1/calculadora.js
@@ -1,16 +1,16 @@
-var btn1 = document.querySelector(#btn1);
+var btn1 = document.querySelector("#btn1");
 btn1.addEventListener("click", sumar);
 
-var btn2 = document.querySelector(#btn2);
+var btn2 = document.querySelector("#btn2");
 btn2.addEventListener("click", restar);
 
-var btn3 = document.querySelector(#btn3);
+var btn3 = document.querySelector("#btn3");
 btn3.addEventListener("click", multiplicar);
 
-var btn4 = document.querySelector(#btn4);
+var btn4 = document.querySelector("#btn4");
 btn4.addEventListener("click", dividir);
 
-var btn5 = document.querySelector(#btn5);
+var btn5 = document.querySelector("#btn5");
 btn5.addEventListener("click", residuo);
 
 //funciones
@@ -20,6 +20,43 @@ function sumar(){
 
   var resInput = Number(num1.value) + Number(num2.value);
 
-  var res = document.querySelector("#btn1");
+  var res = document.querySelector("#resultado");
+  res.value= resInput;
+}
+
+function restar(){
+  var num1 = document.querySelector("#num1");
+  var num2 = document.querySelector("#num2");
+
+  var resInput = Number(num1.value) - Number(num2.value);
+
+  var res = document.querySelector("#resultado");
+  res.value= resInput;
+}
+function multiplicar(){
+  var num1 = document.querySelector("#num1");
+  var num2 = document.querySelector("#num2");
+
+  var resInput = Number(num1.value) * Number(num2.value);
+
+  var res = document.querySelector("#resultado");
+  res.value= resInput;
+}
+function dividir(){
+  var num1 = document.querySelector("#num1");
+  var num2 = document.querySelector("#num2");
+
+  var resInput = Number(num1.value) / Number(num2.value);
+
+  var res = document.querySelector("#resultado");
+  res.value= resInput;
+}
+function residuo(){
+  var num1 = document.querySelector("#num1");
+  var num2 = document.querySelector("#num2");
+
+  var resInput = Number(num1.value) % Number(num2.value);
+
+  var res = document.querySelector("#resultado");
   res.value= resInput;
 }
